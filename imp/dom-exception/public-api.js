d11b5c2d5034d77a2d2ac6fe3d785fdd724ad963
Florian Klampfer <mail@qwtel.com>
2018-06-07 10:58:11 +0700

    Remove DOMException

--- imp/dom-exception/public-api.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// TODO: meeeeehhh
-const DOMException = require("./DOMException").interface;
-
-Object.setPrototypeOf(DOMException.prototype, Error.prototype);
-
-module.exports = { DOMException };
