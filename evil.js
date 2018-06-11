638668aa5a2fd7defe01468cd92ecfa34265d19f
Florian Klampfer <mail@qwtel.com>
2018-06-11 14:36:30 +0700

    Add exports and global hackery

---
+++ evil.js
@@ -0,0 +1,45 @@
+var globalContext = require('./index.js');
+
+//%%
+function getAllKeys( obj ) {
+    var props = [];
+
+    do {
+        props= props.concat(Reflect.ownKeys( obj ));
+    } while ( (obj = Reflect.getPrototypeOf( obj )) && obj !== Object.prototype );
+
+    return props;
+}
+
+//%%
+var keys = getAllKeys(globalContext);
+keys
+
+//%%
+function evilMixin(target, source, keys) {
+  for (let i = 0; i < keys.length; ++i) {
+    if (Reflect.has(target, keys[i])) {
+      continue;
+    }
+
+    // HACK:...
+    let x = source[keys[i]];
+    if (
+      typeof x === 'function'
+      && x.name[0] === String.prototype.toLowerCase.call(x.name[0]) // HACK
+    ) {
+      x = x.bind(target);
+    }
+
+    target[keys[i]] = x;
+    // Reflect.defineProperty(target, keys[i], Reflect.getOwnPropertyDescriptor(source, keys[i]));
+  }
+};
+
+//%%
+
+delete global[Symbol.toStringTag];
+evilMixin(global, globalContext, keys)
+
+//%%
+Object.setPrototypeOf(global, require('./lib/WorkerGlobalScope.js').interface.prototype);
--- index.js
+++ index.js
@@ -1,44 +1,6 @@
 "use strict";
 
-// events
-const Event = require("./lib/Event.js").interface;
-const CustomEvent = require("./lib/CustomEvent.js").interface;
-const ErrorEvent = require("./lib/ErrorEvent.js").interface;
-const MessageEvent = require("./lib/MessageEvent.js").interface;
-const ProgressEvent = require("./lib/ProgressEvent.js").interface;
-
-// event-target
-const EventTarget = require("./lib/EventTarget.js").interface;
-
-// file
-const Blob = require("./lib/Blob.js").interface;
-const File = require("./lib/File.js").interface;
-const FileList = require("./lib/FileList.js").interface;
-const FileReader = require("./lib/FileReader.js").interface;
-
-// abort-controller
-const AbortController = require("./lib/AbortController.js").interface;
-const AbortSignal = require("./lib/AbortSignal.js").interface;
-
-// form-data
-const FormData = require("./lib/FormData.js").interface;
-
-module.exports = {
-  Event,
-  CustomEvent,
-  ErrorEvent,
-  MessageEvent,
-  ProgressEvent,
-
-  EventTarget,
-
-  Blob,
-  File,
-  FileList,
-  FileReader,
-
-  AbortController,
-  AbortSignal,
-
-  FormData
-};
+const WorkerGlobalScope = require('./lib/WorkerGlobalScope.js');
+const globalContext = WorkerGlobalScope.create([]);
+require('./lib/bundle-entry.js').bootstrap('Worker', globalContext, {});
+module.exports = globalContext;
